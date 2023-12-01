e306fc023f8904a2bd03ddc62a53f3a71e52815a
Azat Yalmaev <azatyalmaev@gmail.com>
2023-12-01 13:52:34 +0300

    ура БЛЫДЬ)

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,25 @@
+import os
+import json
+from googleapiclient.discovery import build
 
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj(channel)
+
